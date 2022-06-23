19139c6d87935a8aa0a3964b13155e44d2c2914d
Jonathan YOMBO <jonathan.yombo@din-technology.com>
2022-06-23 18:23:10 +0200

    gestion des fichiers

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Fichier fichier = new Fichier("ruth.txt");
+        fichier.lire();
+    }
+}
